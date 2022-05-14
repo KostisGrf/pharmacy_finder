45f75ebd40c49f5d40badf47a5a7735f9022cf5e
KostisGrf <kostisgratsos@hotmail.gr>
2022-05-15 02:22:23 +0300

    ⚡️Creates map screen

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pharmacy_finder/services/marker_service.dart';
+import 'package:provider/provider.dart';
+
+import '../blocs/application_bloc.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  // BitmapDescriptor? customMarker;
+
+  // Future<void> setCustomMarker() async {
+  //   customMarker = await BitmapDescriptor.fromAssetImage(
+  //       const ImageConfiguration(), 'assets/images/pharmacy-icon.png');
+  // }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // setCustomMarker();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final applicationBloc = Provider.of<ApplicationBloc>(context);
+    final markerService = MarkerService();
+
+    var markers =
+        markerService.getMarkers(applicationBloc.pharmacies);
+    return SafeArea(
+      child: Scaffold(
+        body: GoogleMap(
+          initialCameraPosition: CameraPosition(
+              target: LatLng(
+                  applicationBloc.cityLocation!.geometry.location.lat,
+                  applicationBloc.cityLocation!.geometry.location.lng),
+              zoom: 15.5),
+          zoomGesturesEnabled: true,
+          markers: Set<Marker>.of(markers),
+        ),
+      ),
+    );
+  }
+}
